cf39d2694ef2e7b4d6f9fd72bdc632964b6233d2
codejoncode <jonathanjholloway@gmail.com>
2019-01-11 23:32:45 -0600

    added yellow check marks to the li's  and changed the font color of the li text

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,27 @@ class App extends Component {
       <div className="container">
         <div className="App">
           <nav className="navBar">
-            <h2 className = "whiteText logoText">ID</h2>
+            <h2 className="whiteText logoText">ID</h2>
             <h5>BEST FOREX TRADING PLATFORM</h5>
             <div className="firstPartOfDiv whiteText">
-              <a href="#" className = "whiteText bolder">How it Works</a>
-              <a href="#" className = "whiteText bolder">Range of Markets</a>
-              <a href="#" className = "whiteText bolder">Features &amp; Tools</a>
-              <a href="#" className = "whiteText bolder">Prices</a>
-              <a href="#" className = "whiteText bolder">Training</a>
-              <a href="#" className = "whiteText bolder">Join for free</a>
+              <a href="#" className="whiteText bolder">
+                How it Works
+              </a>
+              <a href="#" className="whiteText bolder">
+                Range of Markets
+              </a>
+              <a href="#" className="whiteText bolder">
+                Features &amp; Tools
+              </a>
+              <a href="#" className="whiteText bolder">
+                Prices
+              </a>
+              <a href="#" className="whiteText bolder">
+                Training
+              </a>
+              <a href="#" className="whiteText bolder">
+                Join for free
+              </a>
             </div>
             <button className="signinButton">SIGN IN</button>
           </nav>
@@ -40,23 +52,44 @@ class App extends Component {
             <div className="column2">
               <h5 className="whiteText">OUR AWARD-WINNING PLATFORM</h5>
               <ul>
-                <li>Leverage from 1:1 to 1:500</li>
-                <li>
+                <li className="bottomChecks"> Leverage from 1:1 to 1:500</li>
+                <li className="bottomChecks">
                   Spread bet or trade CFDs on over 300 major, minor and exotic
                   FX pairs
                 </li>
-                <li>Spreads from just 0.7 points on EUR/USD and USD/JPY</li>
-                <li>Powerful charting and innovative trading tools</li>
-                <li>Trade forex on the move with our mobile and</li>
+                <li className="bottomChecks">
+                  {" "}
+                  Spreads from just 0.7 points on EUR/USD and USD/JPY
+                </li>
+                <li className="bottomChecks">
+                  {" "}
+                  Powerful charting and innovative trading tools
+                </li>
+                <li className="bottomChecks">
+                  {" "}
+                  Trade forex on the move with our mobile and
+                </li>
               </ul>
             </div>
             <div className="column3">
               <ul>
-                <li>Trade forex on the move with our mobile</li>
-                <li>Buy Stop Limit/Sell Stop Limit orders</li>
-                <li>Market depth information</li>
-                <li>Integrated development enviornment for EAs</li>
-                <li>Instant and Market Execution available</li>
+                <li className="bottomChecks">
+                  {" "}
+                  Trade forex on the move with our mobile
+                </li>
+                <li className="bottomChecks">
+                  {" "}
+                  Buy Stop Limit/Sell Stop Limit orders
+                </li>
+                <li className="bottomChecks"> Market depth information</li>
+                <li className="bottomChecks">
+                  {" "}
+                  Integrated development enviornment for EAs
+                </li>
+                <li className="bottomChecks">
+                  {" "}
+                  Instant and Market Execution available
+                </li>
               </ul>
             </div>
           </div>
