50d065db3ffdb5c5ecb15ecab55a9b4094a71f5f
codejoncode <jonathanjholloway@gmail.com>
2019-01-13 00:36:15 -0600

    Updates

---
+++ precache-manifest.945d6575c13b8d9ce7ed2389f0f87f2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2150c4deb8170ff8004d9a357e9eaac6",
+    "url": "/forex_page/static/media/forexcandlesgraph11.2150c4de.PNG"
+  },
+  {
+    "revision": "d851cc605e1df9f02b7e7a68eea1b0d7",
+    "url": "/forex_page/static/media/bottom4.d851cc60.PNG"
+  },
+  {
+    "revision": "d80fec1bdec608628937",
+    "url": "/forex_page/static/js/runtime~main.d80fec1b.js"
+  },
+  {
+    "revision": "45e894c2002762702498",
+    "url": "/forex_page/static/js/main.45e894c2.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/forex_page/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "45e894c2002762702498",
+    "url": "/forex_page/static/css/main.9edfdbe7.chunk.css"
+  },
+  {
+    "revision": "bea0cedf5ff4906883fde1e7f473572a",
+    "url": "/forex_page/index.html"
+  }
+];
