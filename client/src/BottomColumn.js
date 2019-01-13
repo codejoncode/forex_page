464054cca3b6bd83d7cd69c1c93373b141811aa8
codejoncode <jonathanjholloway@gmail.com>
2019-01-12 21:52:04 -0600

    getting the details in the component drawn out good positioning at this point sizing and adding in the other part which will be a background image still needed

--- client/src/BottomColumn.js
+++ client/src/BottomColumn.js
@@ -13,9 +13,25 @@ class BottomColumn extends Component {
           </p>
           <p className="whiteText bolder">&equiv;</p>
         </nav>
-        <div className = "">
-          <p>EUR/USD</p>
+        <div className = "column1Underneath">
+          <p className = "whiteText bolder">EUR/USD</p>
           <p>some text goes here</p>
+          <div>
+              <span><button>BUY</button></span>
+              
+              <span><button>SELL</button></span>
+          </div>
+          <p>some more text</p>
+          <div className = "holdArrows">
+          <span></span>
+          <p className = "medNumber darkBlack">12,230</p>
+          <span className = "arrows"><div><div>&#9650;</div><div>&#9660;</div></div></span>
+          </div>
+          <p>some text</p>
+          <p className = "medNumber whiteText">49,865</p>
+          <p className = "yellowText">price</p>
+          <p className = "medNumber whiteText">1.1893</p>
+
           <div className ="holdImage"></div>
         </div>
       </div>
