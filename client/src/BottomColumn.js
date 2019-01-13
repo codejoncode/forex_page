63dc6f1ada24f88781bf2e06eb630e4a52aa7b6b
codejoncode <jonathanjholloway@gmail.com>
2019-01-12 21:06:56 -0600

    have the columns positioned how I want now need to get the bottom column component completed.

--- client/src/BottomColumn.js
+++ client/src/BottomColumn.js
@@ -3,16 +3,22 @@ import React, { Component } from "react";
 class BottomColumn extends Component {
   render() {
     return (
-      <nav className="navBar secondNav">
-        <p className="whiteText bolder">ID</p>
-        <p className="bolder">
-          <span className="whiteText">BUY</span>
-          <span>/</span>
-          <span className = "darkBlack">SELL</span>
-        </p>
-        <p className = "whiteText bolder">&equiv;</p>
-
-      </nav>
+      <div className="column1">
+        <nav className="navBar secondNav">
+          <p className="whiteText bolder">ID</p>
+          <p className="bolder">
+            <span className="whiteText">BUY</span>
+            <span>/</span>
+            <span className="darkBlack">SELL</span>
+          </p>
+          <p className="whiteText bolder">&equiv;</p>
+        </nav>
+        <div className = "">
+          <p>EUR/USD</p>
+          <p>some text goes here</p>
+          <div className ="holdImage"></div>
+        </div>
+      </div>
     );
   }
 }
--- client/src/BottomSection.js
+++ client/src/BottomSection.js
@@ -5,83 +5,92 @@ class BottomSection extends Component {
   render() {
     return (
       <div className="bottomContainer">
-        <h5 className="whiteText bottomTitle">OUR AWARD-WINNING PLATFORM</h5>
         <div className="bottomDiv">
-          <div className="column1"><BottomColumn /></div>
-          <div className="column2">
-            <ul>
-              <li className="bottomChecks">
-                {" "}
-                <span className="positionSpan">
+          <BottomColumn />
+          <div className="sectionOff">
+            <div className="column2">
+              <h5 className="whiteText bottomTitle">
+                OUR AWARD-WINNING PLATFORM
+              </h5>
+              <ul>
+                <li className="bottomChecks">
                   {" "}
-                  Leverage from 1:1 to 1:500{" "}
-                </span>
-              </li>
-              <li className="bottomChecks">
-                {" "}
-                <span className="positionSpan">
-                  Spread bet or trade CFDs on over 300 major, minor and exotic
-                  FX pairs
-                </span>
-              </li>
-              <li className="bottomChecks">
-                {" "}
-                <span className="positionSpan">
+                  <span className="positionSpan">
+                    {" "}
+                    Leverage from 1:1 to 1:500{" "}
+                  </span>
+                </li>
+                <li className="bottomChecks">
                   {" "}
-                  Spreads from just 0.7 points on EUR/USD and USD/JPY
-                </span>
-              </li>
-              <li className="bottomChecks">
-                {" "}
-                <span className="positionSpan">
+                  <span className="positionSpan">
+                    Spread bet or trade CFDs on over 300 major, minor and exotic
+                    FX pairs
+                  </span>
+                </li>
+                <li className="bottomChecks">
                   {" "}
-                  Powerful charting and innovative trading tools
-                </span>
-              </li>
-              <li className="bottomChecks">
-                {" "}
-                <span className="positionSpan">
+                  <span className="positionSpan">
+                    {" "}
+                    Spreads from just 0.7 points on EUR/USD and USD/JPY
+                  </span>
+                </li>
+                <li className="bottomChecks">
                   {" "}
-                  Trade forex on the move with our mobile and
-                </span>
-              </li>
-            </ul>
-          </div>
-          <div className="column3">
-            <ul>
-              <li className="bottomChecks">
-                {" "}
-                <span className="positionSpan">
+                  <span className="positionSpan">
+                    {" "}
+                    Powerful charting and innovative trading tools
+                  </span>
+                </li>
+                <li className="bottomChecks">
                   {" "}
-                  Trade forex on the move with our mobile
-                </span>
-              </li>
-              <li className="bottomChecks">
-                {" "}
-                <span className="positionSpan">
+                  <span className="positionSpan">
+                    {" "}
+                    Trade forex on the move with our mobile and
+                  </span>
+                </li>
+              </ul>
+            </div>
+            <div className="column3">
+              <br />
+              <br />
+              <ul>
+                <li className="bottomChecks">
                   {" "}
-                  Buy Stop Limit/Sell Stop Limit orders
-                </span>
-              </li>
-              <li className="bottomChecks">
-                {" "}
-                <span className="positionSpan"> Market depth information </span>
-              </li>
-              <li className="bottomChecks">
-                {" "}
-                <span className="positionSpan">
+                  <span className="positionSpan">
+                    {" "}
+                    Trade forex on the move with our mobile
+                  </span>
+                </li>
+                <li className="bottomChecks">
                   {" "}
-                  Integrated development enviornment for EAs
-                </span>
-              </li>
-              <li className="bottomChecks">
-                {" "}
-                <span className="positionSpan">
+                  <span className="positionSpan">
+                    {" "}
+                    Buy Stop Limit/Sell Stop Limit orders
+                  </span>
+                </li>
+                <li className="bottomChecks">
                   {" "}
-                  Instant and Market Execution available
-                </span>
-              </li>
-            </ul>
+                  <span className="positionSpan">
+                    {" "}
+                    Market depth information{" "}
+                  </span>
+                </li>
+                <li className="bottomChecks">
+                  {" "}
+                  <span className="positionSpan">
+                    {" "}
+                    Integrated development enviornment for EAs
+                  </span>
+                </li>
+                <li className="bottomChecks">
+                  {" "}
+                  <span className="positionSpan">
+                    {" "}
+                    Instant and Market Execution available
+                  </span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
